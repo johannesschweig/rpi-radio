0773c28a306a12b026d9f30ef636cf4338b04b39
Johannes Schweig <j.schweig@posteo.de>
2022-01-29 14:25:51 +0100

    3 streams solution (v0.1)
    - hacky shutdown with file
    - nice design with tailwind

--- simple_webserver.py
+++ simple_webserver.py
@@ -1,16 +1,19 @@
 import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
 
 
-# host_name = '192.168.178.44'    # Change this to your Raspberry Pi IP address
+#host_name = '192.168.178.44'
 host_name = '127.0.0.1'
 host_port = 8000
+streams = ['https://stream-relay-geo.ntslive.net/stream',
+    'https://stream-relay-geo.ntslive.net/stream2',
+    'https://www.youtube.com/watch?v=EpjJu7b4UqI' ]
 
 
 class MyServer(BaseHTTPRequestHandler):
-    """ A special implementation of BaseHTTPRequestHander for reading data from
-        and control GPIO of a Raspberry Pi
-    """
+    """ A special implementation of BaseHTTPRequestHander """
+    job = 'test'
 
     def do_HEAD(self):
         """ do_HEAD() can be tested use curl command
@@ -30,21 +33,9 @@ class MyServer(BaseHTTPRequestHandler):
         """ do_GET() can be tested using curl command
             'curl http://server-ip-address:port'
         """
-        html = '''
-            <html>
-            <body style="width:960px; margin: 20px auto;">
-            <h1>Welcome to my Raspberry Pi</h1>
-            <p>Current GPU temperature is {}</p>
-            <form action="/" method="POST">
-                Turn LED :
-                <input type="submit" name="submit" value="On">
-                <input type="submit" name="submit" value="Off">
-            </form>
-            </body>
-            </html>
-        '''
+        html = open('index.html', 'r').read()
         self.do_HEAD()
-        self.wfile.write(html.format('testitest').encode("utf-8"))
+        self.wfile.write(html.encode("utf-8"))
 
     def do_POST(self):
         """ do_POST() can be tested using curl command
@@ -53,12 +44,38 @@ class MyServer(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])    # Get the size of data
         post_data = self.rfile.read(content_length).decode("utf-8")   # Get the data
         post_data = post_data.split("=")[1]    # Only keep the value
+        post_data = post_data.replace('+', ' ')
 
-
-        if post_data == 'On':
-            print('1')
+        # remove previous stream if any
+        if os.path.exists('job.txt'):
+            f = open('job.txt', 'r')
+            self.job = f.read()
+            f.close()
+            subprocess.Popen(['kill', self.job])
+            os.remove('job.txt')
+            # self.job.terminate()
+            print(self.job, 'terminated')
         else:
-            print('2')
+            print('job file does not exist')
+
+        # (optional) start new stream
+        if post_data != 'Ausschalten':
+            stream_id = -1
+            if post_data == 'NTS Live 1':
+                print('playing', 'NTS Live 1')
+                stream_id = 0
+            elif post_data == 'NTS Live 2':
+                print('playing', 'NTS Live 2')
+                stream_id = 1
+            elif post_data == 'Saufen':
+                print('playing', 'Saufen')
+                stream_id = 2
+            self.job = subprocess.Popen(['cvlc', '--play-and-exit', streams[stream_id]])
+            # save pid to file
+            f = open ('job.txt', 'w')
+            f.write(str(self.job.pid))
+            f.close()
+            
         self._redirect('/')    # Redirect back to the root url
 
 
